33ccde27a67d3c5c2bf6c09b8510adacf636d42c
Silmi <129129210+SilmiCamilia@users.noreply.github.com>
2025-09-24 13:10:58 +0700

    ini chimi

--- Test13.java
+++ Test13.java
@@ -2,5 +2,7 @@ package FinalProject13_24;
 public class Test13 {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
+        System.out.println("alip bocah roblox");
+        System.out.println("alip galin selama nya");
     }
 }
